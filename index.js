333d6367179c87abac2406ff53a84d1f37a20bbb
Devdatt Patil <Devdatt.patil@uc.edu>
2022-06-29 01:05:20 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const holes=document.querySelectorAll('.hole');
+const scoreBoard=document.querySelectorAll('.score');
+console.log(scoreBoard);
+const moles=document.querySelectorAll('.mole');
+const btnxyz=document.querySelector('.btn');
+
+
+let lastHole;
+let timeUp=false;
+let score=0;
+let seconds=15;
+
+function randomTime(min,max){
+    return Math.round(Math.random()*(max-min)+min);
+}
+
+function randomHole(holes){
+    const index=Math.floor(Math.random()*holes.length);
+    const hole =holes[index];
+
+    if(hole===lastHole){
+        return randomHole(holes);
+    }
+    lastHole=hole;
+    return hole;
+}
+
+function peep(){
+    const time= randomTime(800,1100);
+    const hole=randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(()=>{
+        hole.classList.remove('up');
+        if(!timeUp){
+            peep();
+        }
+    },time);
+}
+
+function startGame(){
+    scoreBoard.forEach(board=>board.textContent=0);
+    timeUp=false;
+    score=0;
+    peep();
+    seconds=15;
+    setTimeout(()=> timeUp=true,15000) //show random mole for 60sec(timeUp will be true for 60sec)
+    interval=setInterval(updateCount,1000);
+}
+
+function updateCount(){
+    btnxyz.innerHTML=`${seconds}`;
+    if(seconds===1){
+        btnxyz.innerText='Retry'
+        clearInterval(interval);
+    }
+    seconds--;
+}
+
+
+function wack(e){
+    if(!e.isTrusted) return;
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.forEach(board=>board.textContent=score);
+}
+
+moles.forEach(mole=>mole.addEventListener('click',wack));
+
+// function start(d){
+    // if (d.interval){
+        // clearInterval(d.interval);
+        // d.innerHTML='Start';
+    // } else {
+        // d.interval=setInterval(function(){
+        //   refresh here
+        // },10000);
+        // d.innerHTML='Stop';
+    // }
+// }
