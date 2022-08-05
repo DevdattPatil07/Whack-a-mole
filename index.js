417bded32f3325b88e4dc0986e6ba57ba30ba59c
Devdatt Patil <Devdatt.patil@uc.edu>
2022-08-05 18:20:52 +0530

    Updates

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const holes=document.querySelectorAll('.hole');
-const scoreBoard=document.querySelectorAll('.score');
+const scoreBoard=document.querySelector('.score');
 const moles=document.querySelectorAll('.mole');
 const btnxyz=document.querySelector('.btn');
 
+let highscore=localStorage.getItem("HighScore");
+
+if(highscore){
+    updateHighScore(highscore);
+}
 
 let lastHole;
 let timeUp=false;
 let score=0;
-let seconds=15;
+let seconds=document.getElementById("time").value;
+let difficulty=document.getElementById("speed").value;
 
+//mole peep time
 function randomTime(min,max){
     return Math.round(Math.random()*(max-min)+min);
 }
 
+//randome hole selector function
 function randomHole(holes){
     const index=Math.floor(Math.random()*holes.length);
     const hole =holes[index];
@@ -24,45 +32,62 @@ function randomHole(holes){
     return hole;
 }
 
-function peep(){
-    const time= randomTime(800,1100);
+//mole peeping function taking argument as speed
+function peep(low,high){
+    const time= randomTime(low,high);
     const hole=randomHole(holes);
     hole.classList.add('up');
     setTimeout(()=>{
         hole.classList.remove('up');
         if(!timeUp){
-            peep();
+            peep(low,high);
         }
     },time);
 }
 
 function startGame(){
-    scoreBoard.forEach(board=>board.textContent=0);
+    scoreBoard.textContent=0;
     timeUp=false;
     score=0;
-    peep();
-    seconds=15;
-    setTimeout(()=> timeUp=true,15000) //show random mole for 15sec(timeUp will be true for 15sec)
+    difficulty=document.getElementById("speed").value;
+    seconds=document.getElementById("time").value;
+    let n=3-difficulty+1;
+    peep(n*800,n*1100);
+    setTimeout(()=> timeUp=true,seconds*1000) //show random mole for 15sec(timeUp will be true for 15sec)
     interval=setInterval(updateCount,1000);
 }
 
 //counter function
 function updateCount(){
     btnxyz.innerHTML=`${seconds}`;
-    if(seconds===1){
-        btnxyz.innerText='Retry'
+    if(seconds===0){
+        btnxyz.innerText="Retry ⟳"
         clearInterval(interval);
     }
     seconds--;
 }
 
-
+//wack function executed only when you hit mole
 function wack(e){
-    console.log(e);
+    var child=this.childNodes;
+    console.log(child[1]);
+    child[1].style="opacity:1";
+    setTimeout(() => {
+        child[1].style.opacity="0";
+       }, 500);
     if(!e.isTrusted) return;
     score++;
+    if(score>highscore){
+        highscore=score;
+        updateHighScore(highscore);
+    }
     this.parentNode.classList.remove('up');
-    scoreBoard.forEach(board=>board.textContent=score);
+    scoreBoard.textContent=score;
 }
 
 moles.forEach(mole=>mole.addEventListener('click',wack));
+
+function updateHighScore(highscore){
+    document.getElementById("highScore").innerText=highscore;
+    localStorage.setItem("HighScore",highscore);
+}
